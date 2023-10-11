186db6c8354cfe15ae98cc19697da4e93122678e
이다영 <yooung513@gmail.com>
2023-10-11 17:03:30 +0900

    2-1~2 실습

---
+++ javajungsuk/javajungsuk/src/ch2/Ex1.java
@@ -0,0 +1,14 @@
+package ch2;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        System.out.println("5+3");      // 문자로 출력
+        System.out.println(5+3);        // 덧셈
+        System.out.println(5-3);        // 뺄셈
+        System.out.println(5*3);        // 곱셈
+        System.out.println(5/3);        // 나눗셈 (몫 값을 출력)
+        System.out.println(5/3.0);      // 나눗셈 (계산 값을 출력)
+    }
+}
