105319fc4edf6648d2bc59555d35697ddebbb8ab
이다영 <yooung513@gmail.com>
2023-10-25 23:43:06 +0900

    4-5~8. if-else문, 중첩 if문

--- javajungsuk/javajungsuk/src/ch4/Ex4.java
+++ javajungsuk/javajungsuk/src/ch4/Ex4.java
@@ -1,2 +1,27 @@
-package ch4;public class Ex4 {
+package ch4;
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        int score = 0;      // 점수 저장 변수
+        char grade = ' ';   // 학점 저장 변수 (공백으로 초기화)
+
+        System.out.print("점수를 입력하세요.");
+
+        Scanner sc = new Scanner(System.in);
+        score = sc.nextInt();   // 화면을 통해 입력받은 숫자를 score에 저장
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+
+        System.out.println("당신의 학점은 " + grade + "입니다.");
+    }
 }
--- javajungsuk/javajungsuk/src/ch4/Ex5.java
+++ javajungsuk/javajungsuk/src/ch4/Ex5.java
@@ -1,2 +1,45 @@
-package ch4;public class Ex5 {
+package ch4;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        int score = 0;                  // 점수 저장 변수
+        char grade = ' ', opt = ' ';    // 학점 저장 변수 (공백으로 초기화)
+
+
+        System.out.print("점수를 입력하세요.");
+
+        Scanner sc = new Scanner(System.in);
+        score = sc.nextInt();   // 화면을 통해 입력받은 숫자를 score에 저장
+
+        System.out.printf("당신의 점수는 %d입니다. %n", score);
+
+        if (score >= 90) {
+            grade = 'A';
+            if (score >= 98) {
+                opt = '+';
+            } else if (score < 94) {
+                opt = '-';
+            }
+        } else if (score >= 80) {
+            grade = 'B';
+            if (score >= 88) {
+                opt = '+';
+            } else if (score < 84) {
+                opt = '-';
+            }
+        } else if (score >= 70) {
+            grade = 'C';
+            if (score >= 78) {
+                opt = '+';
+            } else if (score < 74) {
+                opt = '-';
+            }
+        } else {
+            grade = 'D';
+        }
+
+        System.out.println("당신의 학점은 " + grade + opt + "입니다.");
+    }
 }
