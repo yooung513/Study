c5fa7902b047469d0b24fc26d7767b67a282b695
이다영 <yooung513@gmail.com>
2023-10-31 16:34:50 +0900

    4-15. 중첩 for문

---
+++ javajungsuk/javajungsuk/src/ch4/Ex15.java
@@ -0,0 +1,33 @@
+package ch4;
+
+import java.util.Arrays;
+
+public class Ex15 {
+    public static void main(String[] args) {
+
+        // 구구단
+        for (int i=2; i<=9; i++){
+            for (int j=1; j<=9; j++){
+                System.out.println(i+"*"+j+"="+(i*j));
+            }
+            System.out.println();
+        }
+
+
+        // 별찍기
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
