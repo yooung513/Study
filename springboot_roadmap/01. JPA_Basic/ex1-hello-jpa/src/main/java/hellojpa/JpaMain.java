dcd7a86ac28ba0fd32dfc757fcfda0725affc514
이다영 <yooung513@gmail.com>
2023-11-06 15:23:42 +0900

    JPA Test

--- springboot_roadmap/01. JPA_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ springboot_roadmap/01. JPA_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,10 @@
 package hellojpa;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaMain {
     public static void main(String[] args) {
-
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
     }
 }
