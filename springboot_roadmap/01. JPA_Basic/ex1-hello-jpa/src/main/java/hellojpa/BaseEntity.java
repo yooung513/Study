8c0a43b0325d41b4926ed1cc7eb85e7868e01388
이다영 <yooung513@gmail.com>
2023-11-09 13:22:35 +0900

    7-1. 고급 매핑 - 상속관계 매핑

---
+++ springboot_roadmap/01. JPA_Basic/ex1-hello-jpa/src/main/java/hellojpa/BaseEntity.java
@@ -0,0 +1,14 @@
+package hellojpa;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+
+    private String createBy;
+    private LocalDateTime createdDate;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedDate;
+
+
+    
+}
---
+++ springboot_roadmap/01. JPA_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain_EX6.java
@@ -0,0 +1,54 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain_EX5 {
+
+    // 고급 매핑전략
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            Movie movie = new Movie();
+            movie.setDirector("봉준호");
+            movie.setActor("송강호");
+            movie.setName("기생충");
+            movie.setPrice(10000);
+
+            em.persist(movie);
+
+            em.flush();
+            em.clear();
+
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println();
+
+
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private static Member saveMember(EntityManager em) {
+        Member member = new Member();
+        member.setName("member1");
+
+        em.persist(member);
+        return member;
+    }
+}
