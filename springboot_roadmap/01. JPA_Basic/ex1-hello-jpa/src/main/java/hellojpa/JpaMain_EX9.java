19f269cd6e3c96f3135b17b145aa9d1ed0c40a7a
이다영 <yooung513@gmail.com>
2023-11-10 11:07:30 +0900

    8-3. 프록시 - 영속성 전이와 고아 객체

--- springboot_roadmap/01. JPA_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain_EX9.java
+++ springboot_roadmap/01. JPA_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain_EX9.java
@@ -1,15 +1,16 @@
 package hellojpa;
 
+import org.checkerframework.checker.units.qual.C;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
-import java.util.Locale;
 
-public class JpaMain_EX8 {
+public class JpaMain_EX9 {
 
-    // 프록시 - 즉시로딩과 지연 로딩
+    // 프록시 - 영속성 전이와 고아 객체
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -19,43 +20,33 @@ public class JpaMain_EX8 {
         tx.begin();
 
         try {
-            Team team1 = new Team();
-            team1.setName("teamA");
-            em.persist(team1);
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
+            // 영속성 전이
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.setTeam(team1);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member2 = new Member();
-            member2.setName("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
+//            em.persist(child1);       // 영속성 전이시 부모 엔티티가 저장될 때
+//            em.persist(child2);       // 자동으로 함께 저장됨
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-            Member findMem = em.find(Member.class, member1.getId());
 
-            // 지연 로딩 = 프록시 객체 / 즉시 로딩 = Team 객체
-            System.out.println("findMem = " + findMem.getTeam().getClass());
+            // 고아 객체
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);    // 고아 객체 생성 -> 첫번째 객체 제거됨
 
+            em.remove(findParent);      // 부모 객체가 지워짐에 따라 자식객체 또한 함께 지워짐
 
-            // 즉시로딩 사용 시 문제점 : JPQL에서 N+1 문제 발생
-            // member2 query가 나가고, 즉시로딩에 의해 team을 조회하는 쿼리또한 같이 나가게 됨
-            // SQL : select * from member2
-            // SQL : select * from team1 where team_id = ?       => 즉시로딩을 지양해야하는 이유
-            List<Member> members = em.createQuery("select m from Member m", Member.class)
-                            .getResultList();
 
             tx.commit();
 
-        } catch (Exception e) pu
+        } catch (Exception e) {
             System.out.println("e = " + e);    // 에러 메세지 출력
             tx.rollback();
         } finally {
