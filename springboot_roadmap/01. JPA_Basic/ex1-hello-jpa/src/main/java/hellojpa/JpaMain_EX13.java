b3a746068240d116dc28758d30d970c459094d74
이다영 <yooung513@gmail.com>
2023-11-13 20:27:49 +0900

    10-1. JPQL 소개

--- springboot_roadmap/01. JPA_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain_EX13.java
+++ springboot_roadmap/01. JPA_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain_EX13.java
@@ -4,12 +4,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
 public class JpaMain_EX13 {
 
-    // 값 타입 - 값 타입 컬렉션
+    // 객체지향 쿼리 언어 (JPQL)
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -20,64 +23,35 @@ public class JpaMain_EX13 {
 
         try {
 
-            // 값 타입 저장 예제
-            Member member = new Member();
-            member.setName("user1");
-            member.setHomeAddress(new Address("home city", "street", "zipcode"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressList().add(new Address("old city A", "street", "zipcode"));
-            member.getAddressList().add(new Address("old city B", "street", "zipcode"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
+            // JPQL 기본
+            List<Member> result = em.createQuery(
+                    "select m From Member m where m.username like '%kim%'",
+                    Member.class
+            ).getResultList();
 
 
-            // 값 타입 조회 예제
-            Member findMember = em.find(Member.class, member.getId());  // 지연로딩으로 값을 조회함
+            // Criteria
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            // 해당 값을 호출할 때 쿼리문 호출 (지연 로딩)
-            List<Address> findAddressList = findMember.getAddressList();
-            for (Address address : findAddressList) {
-                System.out.println("address_city = " + address.getCity());
+            Root<Member> from = query.from(Member.class);
+
+            CriteriaQuery<Member> cq = query.select(from);
+            String username = "lee";
+            if (username != null) {
+                cq = cq.where(cb.equal(from.get("username"), "kim"));
             }
 
-            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-            for (String favoriteFood : favoriteFoods) {
-                System.out.println("favoriteFood = " + favoriteFood);
-            }
-
-            em.flush();
-            em.clear();
+            List<Member> resultList = em.createQuery(cq)
+                    .getResultList();
 
 
-            // 값 타입 수정 예제 -> 일부 변경이 아닌 완전 교체로 수정해야 함
-            Member findMem = em.find(Member.class, member.getId());
-
-            // homeCity -> newCity
-//            findMem.getHomeAddress().setCity("newCity"); // 값 참조 변경됨 -> 불변 객체로 변경
-            Address oldAddress = findMem.getHomeAddress();
-            findMem.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipCode()));
-
-            // 값 타입 컬렉션 수정
-            // 치킨 -> 돈까스
-            findMem.getFavoriteFoods().remove("치킨");
-            findMem.getFavoriteFoods().add("돈까스");
-
-            // old city A -> new city A
-
-            findMem.getAddressList().remove(new Address("old city A", "street", "zipcode"));
-            findMem.getAddressList().add(new Address("new city A", "street", "zipcode"));
+            // QueryDSL -> 셋팅 필요 (실무 사용 권장)
 
 
-
-            // 값 타입 컬렉션의 대안 방법 - 일대다 매핑
-            findMem.getAddressHistory().add(new AddressEntity("new city A", "street", "zipcode"));
+            // 네이티브 SQL
+            List resultNativeSQL = em.createNativeQuery("select MEMBER_ID, city, streete, zipcode, USERNAME from MEMBER")
+                    .getResultList();
 
 
             tx.commit();
