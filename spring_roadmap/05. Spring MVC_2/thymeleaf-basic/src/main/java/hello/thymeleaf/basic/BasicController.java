64ad03167b256e795f7eccee99ddb121d139b667
이다영 <yooung513@gmail.com>
2023-11-23 15:34:29 +0900

    URL 링크

--- spring_roadmap/05. Spring MVC_2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ spring_roadmap/05. Spring MVC_2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -77,6 +77,16 @@ public class BasicController {
     }
 
 
+    // URL 링크
+    @GetMapping("/link")
+    public String link(Model model) {
+
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
     @Data
     static class User{
 
